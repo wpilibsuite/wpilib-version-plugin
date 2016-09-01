b26d29c47170d833149e29b94590847f584d9262
Fredric Silberberg <fred@silberberg.xyz>
2016-08-31 19:32:58 -0700

    Added the version regex and the grgit dependency

--- src/main/groovy/edu/wpi/first/wpilib/versioning/WPILibVersioningPlugin.groovy
+++ src/main/groovy/edu/wpi/first/wpilib/versioning/WPILibVersioningPlugin.groovy
@@ -10,6 +10,34 @@ import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
  * determined based on git
  */
 class WPILibVersioningPlugin implements Plugin<Project> {
+    //^(?()?(-(?<dirty>dirty))?$
+    // A valid version string from git describe takes the form of v1.0.0-beta-2-1-gbd478ea-dirty, where
+    // everything after the v1.0.0 part is optional. Below, I break each piece out in it's own string, and then put
+    // the final regex together
+
+    // This is the only required part of the version. This captures a 'v', then 3 numbers separated by '.'.
+    // This introduces a capturing group for the main version number called 'version'.
+    static final String mainVersion = /v(?<version>[0-9]+\.[0-9]+\.[0-9]+)/
+
+    // This is the beta/rc qualifier. It is a '-', followed by either 'beta' or 'rc', followed by another '-', finally
+    // followed by the beta/rc number. This introduces a capturing group for the qualifier number, called 'qualifier'.
+    static final String qualifier = /-(?<qualifier>(beta|rc)-[0-9]+)/
+
+    // This is the number of commits since the last annotated tag, and the commit hash of the latest commit. This is
+    // a '-', followed by a number, the number of commits, followed by a '-', followed by a 'g', followed by the git
+    // abbreviation of the hash of the current commit. This introduces 2 capturing groups, one for the number of
+    // commits, 'commits', and one for the commit hash, 'sha'.
+    static final String commits = /-(?<commits>[0-9]+)-(?<sha>g[a-f0-9]+)/
+
+    // This is whether or not there are changes in any of the files tracked by git at the time of version number
+    // generation. This is a '-', followed by the string 'dirty'. This introduces a capturing group, called 'dirty'.
+    static final String dirty = /-(?<dirty>dirty)/
+
+    // This is the final regex. mainVersion is the only element that is required. Each subpart, if it shows up, must
+    // show up in full. A fully expanded version of the regex is copied below:
+    // ^v(?<version>[0-9]+\.[0-9]+\.[0-9]+)(-(?<qualifier>(beta|rc)-[0-9]+))?(-(?<commits>[0-9]+)-(?<sha>g[a-f0-9]+))?(-(?<dirty>dirty))?$
+    static final String versionRegex = "^$mainVersion($qualifier)?($commits)?($dirty)?\$"
+
     @Override
     void apply(Project project) {
         project.extensions.add("WPILibVersion", WPILibVersioningPluginExtension)
